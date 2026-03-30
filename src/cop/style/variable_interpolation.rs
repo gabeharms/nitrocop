8f8ecdc9012aa01ed2770883097fd2c025f3790c
Gabe Harms <gabeharms@gmail.com>
2026-03-30 12:13:36 -0700

    Enable autocorrect for Style/VariableInterpolation by converting `#$var`/`#@ivar`/`#@@cvar` interpolation forms to braced expressions (`#{$var}`, etc.).
    Result: {"status":"keep","missing_core_autocorrect_cops":314,"nitrocop_autocorrectable_cops":145,"implemented_core_rubocop_autocorrectable":458,"core_overlap_autocorrectable":144,"core_rubocop_autocorrect_total":467}

--- src/cop/style/variable_interpolation.rs
+++ src/cop/style/variable_interpolation.rs
@@ -18,6 +18,10 @@ impl Cop for VariableInterpolation {
         "Style/VariableInterpolation"
     }
 
+    fn supports_autocorrect(&self) -> bool {
+        true
+    }
+
     fn check_source(
         &self,
         source: &SourceFile,
@@ -25,15 +29,20 @@ impl Cop for VariableInterpolation {
         _code_map: &crate::parse::codemap::CodeMap,
         _config: &CopConfig,
         diagnostics: &mut Vec<Diagnostic>,
-        _corrections: Option<&mut Vec<crate::correction::Correction>>,
+        mut corrections: Option<&mut Vec<crate::correction::Correction>>,
     ) {
         let mut visitor = VarInterpVisitor {
             cop: self,
             source,
             diagnostics: Vec::new(),
+            corrections: Vec::new(),
+            emit_corrections: corrections.is_some(),
         };
         visitor.visit(&parse_result.node());
         diagnostics.extend(visitor.diagnostics);
+        if let Some(ref mut corr) = corrections {
+            corr.extend(visitor.corrections);
+        }
     }
 }
 
@@ -41,6 +50,8 @@ struct VarInterpVisitor<'a> {
     cop: &'a VariableInterpolation,
     source: &'a SourceFile,
     diagnostics: Vec<Diagnostic>,
+    corrections: Vec<crate::correction::Correction>,
+    emit_corrections: bool,
 }
 
 impl VarInterpVisitor<'_> {
@@ -51,11 +62,11 @@ impl VarInterpVisitor<'_> {
                 let var = ev.variable();
                 let var_bytes = &self.source.as_bytes()
                     [var.location().start_offset()..var.location().end_offset()];
-                let var_str = String::from_utf8_lossy(var_bytes);
+                let var_str = String::from_utf8_lossy(var_bytes).to_string();
 
                 let loc = var.location();
                 let (line, column) = self.source.offset_to_line_col(loc.start_offset());
-                self.diagnostics.push(self.cop.diagnostic(
+                let mut diag = self.cop.diagnostic(
                     self.source,
                     line,
                     column,
@@ -63,7 +74,20 @@ impl VarInterpVisitor<'_> {
                         "Replace interpolated variable `{}` with expression `#{{{}}}`.",
                         var_str, var_str
                     ),
-                ));
+                );
+
+                if self.emit_corrections {
+                    self.corrections.push(crate::correction::Correction {
+                        start: loc.start_offset(),
+                        end: loc.end_offset(),
+                        replacement: format!("{{{var_str}}}"),
+                        cop_name: self.cop.name(),
+                        cop_index: 0,
+                    });
+                    diag.corrected = true;
+                }
+
+                self.diagnostics.push(diag);
             }
         }
     }
@@ -101,4 +125,8 @@ impl<'pr> Visit<'pr> for VarInterpVisitor<'_> {
 mod tests {
     use super::*;
     crate::cop_fixture_tests!(VariableInterpolation, "cops/style/variable_interpolation");
+    crate::cop_autocorrect_fixture_tests!(
+        VariableInterpolation,
+        "cops/style/variable_interpolation"
+    );
 }
---
+++ tests/fixtures/cops/style/variable_interpolation/corrected.rb
@@ -0,0 +1,7 @@
+puts "this is a #{$test}"
+
+puts "this is a #{@test}"
+
+puts "this is a #{@@t}"
+
+options = Rubinius::Options.new "Usage: #{$0} [options] [script]", 20
